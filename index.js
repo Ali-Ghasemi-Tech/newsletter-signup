d0876caf95dfe3727542f8b90e0d6a052d5886ae
TLB30 <thelastblade30@gmail.com>
2023-08-18 17:53:51 +0330

    add success screen and dismiss button

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 const invalidText = document.getElementById('email-invalid-text');
 const input = document.getElementById('input');
 const button = document.getElementById('signup-button');
+const successScreen = document.getElementById('success-popup');
+const successText = document.getElementById('success-text');
+const dismiss =document.getElementById('dismiss-button');
+const success = document.getElementById('success');
+let validEmail;
 
 button.addEventListener('click' , () =>{
     let email = String(input.value);
     email = email.split('');
     
     if(!validateEmail(email)){
+        success.classList.add('hidden');
         invalidText.classList.remove('hidden');
         input.style.backgroundColor = "rgb(255 237 213)";
         input.style.color ="rgb(194 65 12)";
         input.style.borderColor = "rgb(251 146 60)";
-    } else if(validateEmail(email)) {
-        // go to the success page
-        console.log('succuss')
+    } else if(validateEmail(email) && validEmail !== input.value) {
+        const span =  document.createElement('span');
+        span.textContent = `A confirmation email has been sent to ${input.value}. 
+        Please open it and click the button inside to confirm your subscription.`
+        successText.appendChild(span);
+        successScreen.classList.remove('translate-y-full');
+
+        success.classList.remove('hidden');
+        invalidText.classList.add('hidden');
+        input.style.backgroundColor = "rgb(187 247 208)";
+        input.style.color ='green';
+        input.style.borderColor = "rgb(156 163 175)";
+        // signup just once
+        validEmail = input.value;
     }
-    console.log(validateEmail(email));
+    
 }); 
+
+dismiss.addEventListener('click' , () =>{
+    successScreen.classList.add('translate-y-full');
+    span.remove();
+});
 
 
 function validateEmail(email) {
