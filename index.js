b6c3215eca5bebdea41330710779029e648f06bf
TLB30 <thelastblade30@gmail.com>
2023-08-18 17:59:55 +0330

    add already sigend up system

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ const successScreen = document.getElementById('success-popup');
 const successText = document.getElementById('success-text');
 const dismiss =document.getElementById('dismiss-button');
 const success = document.getElementById('success');
+const already = document.getElementById('already-done');
 let validEmail;
 
 button.addEventListener('click' , () =>{
+    let span = '';
     let email = String(input.value);
     email = email.split('');
     
     if(!validateEmail(email)){
         success.classList.add('hidden');
         invalidText.classList.remove('hidden');
+        already.classList.add('hidden');
+
         input.style.backgroundColor = "rgb(255 237 213)";
         input.style.color ="rgb(194 65 12)";
         input.style.borderColor = "rgb(251 146 60)";
     } else if(validateEmail(email) && validEmail !== input.value) {
-        const span =  document.createElement('span');
+        span =  document.createElement('span');
         span.textContent = `A confirmation email has been sent to ${input.value}. 
         Please open it and click the button inside to confirm your subscription.`
         successText.appendChild(span);
@@ -26,13 +30,19 @@ button.addEventListener('click' , () =>{
 
         success.classList.remove('hidden');
         invalidText.classList.add('hidden');
+        already.classList.add('hidden');
+
         input.style.backgroundColor = "rgb(187 247 208)";
         input.style.color ='green';
         input.style.borderColor = "rgb(156 163 175)";
         // signup just once
         validEmail = input.value;
     }
-    
+    else if(validEmail === input.value){
+        already.classList.remove('hidden');
+        success.classList.add('hidden');
+        invalidText.classList.add('hidden');
+    }
 }); 
 
 dismiss.addEventListener('click' , () =>{
