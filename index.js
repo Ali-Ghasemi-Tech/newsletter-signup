3f4017b26c174191ac31bcfe5b0e7913881f875d
TLB30 <thelastblade30@gmail.com>
2023-08-19 20:12:51 +0330

    add success pop up on desktop version and make email bold

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const successText = document.getElementById('success-text');
 const dismiss =document.getElementById('dismiss-button');
 const success = document.getElementById('success');
 const already = document.getElementById('already-done');
+const bg = document.getElementById('success-bg');
 let validEmail;
 
 button.addEventListener('click' , () =>{
@@ -27,11 +28,16 @@ button.addEventListener('click' , () =>{
             let child = successText.lastElementChild;
             successText.removeChild(child);
         }
+        const email = input.value;
         const span =  document.createElement('span');
-        span.textContent = `A confirmation email has been sent to ${input.value}. 
+        span.innerHTML = `A confirmation email has been sent to ${email.bold()}. 
         Please open it and click the button inside to confirm your subscription.`
         successText.appendChild(span);
+
+        bg.classList.remove('opacity-0');
+        bg.classList.remove('-z-10');
         successScreen.classList.remove('translate-y-full');
+        successScreen.classList.remove('md:translate-y-[200%]');
 
         success.classList.remove('hidden');
         invalidText.classList.add('hidden');
@@ -47,11 +53,15 @@ button.addEventListener('click' , () =>{
         already.classList.remove('hidden');
         success.classList.add('hidden');
         invalidText.classList.add('hidden');
+
     }
 }); 
 
 dismiss.addEventListener('click' , () =>{
     successScreen.classList.add('translate-y-full');
+    successScreen.classList.add('md:translate-y-[200%]');
+    bg.classList.add('opacity-0');
+    bg.classList.add('-z-10');
 });
 
 
